faac8aaf2ed4eae16eb82ba87633f2a191fefca3
Caleb Reinking <calebreinking@Kion.local>
2020-04-14 17:22:49 -0400

    reordering egm table, adding popovers

--- js/egm.js
+++ js/egm.js
@@ -1,27 +1,27 @@
 /////// CONSTANTS ///////////
 const PSE_VALUES = {
-  "Ability to Influence Policy": 0,
-  "Efficiency and Effectiveness": 1,
-  "Flexibility and Pace": 2,
-  "Innovation, Expertise, and Capabilities": 3,
-  "Scale, Sustainability, and Reach": 4
+  "Scale, Sustainability, and Reach": 0,
+  "Ability to Influence Policy": 1,
+  "Innovation, Expertise, and Capabilities": 2,
+  "Efficiency and Effectiveness": 3,
+  "Flexibility and Pace": 4,
 };
 
 const PSE_UNITAID_VALUES = {
-  "Reputation and Credible Convening Power": 5,
-  "Risk-Mitigation and Flexible Authorities": 6,
+  "Strong In-Country Networks and Relationships": 5,
+  "Support to Strengthen Enabling Environments": 6,
   "Sectoral Expertise and Knowledge": 7,
-  "Strong In-Country Networks and Relationships": 8,
-  "Support to Strengthen Enabling Environments": 9
+  "Risk-Mitigation and Flexible Authorities": 8,
+  "Reputation and Credible Convening Power": 9,
 };
 
 const WAYS_WE_ENGAGE = {
-  "Advancing Learning and Market Research;": 0,
-  "Catalyzing Private-Sector Resources;": 1,
+  "Information-Sharing and Strategic Alignment": 0,
+  "Advancing Learning and Market Research;": 1,
   "Harnessing Private-Sector Expertise and Innovation;": 2,
-  "Information-Sharing and Strategic Alignment": 3,
+  "Catalyzing Private-Sector Resources;": 3,
   "Strengthening the Enabling Environment": 4,
-  "Unlocking Private Investment;": 5
+  "Unlocking Private Investment;": 3
 };
 
 
@@ -202,6 +202,8 @@ const map = {
   template: '#map-component',
   mounted: function() {
     this.filter_records(this.filtered_documents)
+    $('[data-toggle="popover"]').popover()
+    $('[data-toggle="tooltip"]').tooltip()
   },
   watch: {
     // whenever question changes, this function will run
